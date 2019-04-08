bef59c0034533f1a712490653c36dc1072351c23
Amrita <amritanair@ccs.neu.edu>
2019-04-08 18:20:10 -0400

    Minor fixes

--- data/db.js
+++ data/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const databaseName = 'white-board';
+const databaseName = 'recipe-box';
 var connectionString = 'mongodb://localhost/';
 connectionString += databaseName;
-mongoose.connect('mongodb://heroku_w63skhr5:qdes5uqp45b85b6tbgq8jrp5m9@ds131676.mlab.com:31676/heroku_w63skhr5', { useNewUrlParser: true });
+mongoose.connect(connectionString, { useNewUrlParser: true });
