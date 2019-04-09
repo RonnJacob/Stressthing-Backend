561aaa5c3921732da126c8882335ab77fff55966
Ronn <jacob.r@husky.neu.edu>
2019-04-09 15:27:36 -0400

    Implemented  generic user services

--- data/services/user.services.js
+++ data/services/user.services.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 const userDao = require('../daos/user.dao.server')
 
-let userId = 1;
-
 const createUser = (req, res) =>{
     var user = req.body;
-    // user['_id'] = userId++;
     userDao.createUser(user).then(user => res.send(user));
 };
 
@@ -13,8 +10,23 @@ const findUsers = (req, res) => {
     userDao.findAllUser().then(users => res.send(users));
 };
 
+const findUserById = (req, res) => {
+    userDao.findUserById(req.params.id).then(user => res.send(user));
+};
+
+const updateUser = (req, res) => {
+    userDao.updateUser(req.params.id, req.body).then(user => res.send(user));
+};
+
+const deleteUser = (req, res) => {
+    userDao.deleteUser(req.params.id).then(res.send('User deleted from the user database.'));
+};
+
 
 module.exports = function(app) {
     app.post('/api/user', createUser);
     app.get('/api/user', findUsers);
+    app.get('/api/user/:id', findUserById);
+    app.put('/api/user/:id', updateUser);
+    app.delete('/api/user/:id', deleteUser);
 };
