4aa9dd9b3025a3462ae2e86ae73ccb73fa3039e6
Ronn <jacob.r@husky.neu.edu>
2019-04-22 01:36:20 -0400

    useMongoClient flag set

--- data/db.js
+++ data/db.js
@@ -5,5 +5,5 @@ module.exports = function(){
     //     // connectionString += databaseName;
     var connectionString =
         'mongodb://heroku_xkx7wdmr:3fhd03cse9lkkgoe1t0s8nnopb@ds145456.mlab.com:45456/heroku_xkx7wdmr';
-    mongoose.connect(connectionString, { useNewUrlParser: true });
+    mongoose.connect(connectionString, { useNewUrlParser: true , useMongoClient: true});
 };
