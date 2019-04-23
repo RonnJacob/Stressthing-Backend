4e9cfb1fb4d48c622e6da7ec9bfe876a4da402e1
Ronn <jacob.r@husky.neu.edu>
2019-04-23 19:12:40 -0400

    Testing deployment

--- data/db.js
+++ data/db.js
@@ -2,8 +2,8 @@ module.exports = function(){
     const mongoose = require('mongoose');
     const databaseName = 'recipe-box';
     // var connectionString = 'mongodb://localhost/';
-    //     // connectionString += databaseName;
+    //     connectionString += databaseName;
     var connectionString =
         'mongodb://heroku_xkx7wdmr:3fhd03cse9lkkgoe1t0s8nnopb@ds145456.mlab.com:45456/heroku_xkx7wdmr';
-    mongoose.connect(connectionString, { useNewUrlParser: true , useMongoClient: true});
+    mongoose.connect(connectionString, { useNewUrlParser: true, useMongoClient: true });
 };
