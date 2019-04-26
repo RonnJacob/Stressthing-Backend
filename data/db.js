776e260029d1c30f9b3a99475454ce89e66678cb
Ronn <jacob.r@husky.neu.edu>
2019-04-26 00:41:03 -0400

    Deploying on heroku

--- data/db.js
+++ data/db.js
@@ -1,9 +1,9 @@
 module.exports = function(){
     const mongoose = require('mongoose');
     const databaseName = 'recipe-box';
-    var connectionString = 'mongodb://localhost/';
-         connectionString += databaseName;
-    // var connectionString =
-    //     'mongodb://heroku_xkx7wdmr:3fhd03cse9lkkgoe1t0s8nnopb@ds145456.mlab.com:45456/heroku_xkx7wdmr';
-    mongoose.connect(connectionString, { useNewUrlParser: true});
+    // var connectionString = 'mongodb://localhost/';
+    //      connectionString += databaseName;
+    var connectionString =
+        'mongodb://heroku_xkx7wdmr:3fhd03cse9lkkgoe1t0s8nnopb@ds145456.mlab.com:45456/heroku_xkx7wdmr';
+    mongoose.connect(connectionString, { useNewUrlParser: true, useMongoClient: true});
 };
