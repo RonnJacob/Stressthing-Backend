66a6c953da55834e1a021e5a5927159c6c698208
Ronn <jacob.r@husky.neu.edu>
2019-10-19 17:18:19 -0400

    Modified for dummy data push

--- create_dataset.py
+++ create_dataset.py
@@ -1,3 +1,5 @@
+import json
+import requests
 import random
 from faker import Faker
 
@@ -12,6 +14,22 @@ from randomtimestamp import randomtimestamp
 f = open("record.txt", "w")
 list1 = ["Biking" , "Running", "Walking", "Rest", "Swimming"]
 
-for i in range(1, 100):
-    f.write("0" + ", " + str(fake.date_between(start_date=start_date, end_date='+1m')) + ", " + str(random.choice(list1) ) + ", " + str(random.randint(60, 100)) + ", " + str(random.randint(100, 130)))
-    f.write("\n")
+#for i in range(1, 100):
+#    f.write("0" + ", " + str(fake.date_between(start_date=start_date, end_date='+1m')) + ", " + str(random.choice(list1) ) + ", " + str(random.randint(60, 100)) + ", " + str(random.randint(100, 130)))
+#    f.write("\n")
+
+
+#print(fake.date_between(start_date=start_date, end_date='+1m'))
+for i in range(1,1000):
+    fitness_data = {}
+    fitness_data['timestamp']=str(fake.date_between(start_date=start_date, end_date='+1m'))
+    fitness_data['activity']=str(random.choice(list1))
+    fitness_data['currentHeartRate']=str(random.randint(100,130))
+    fitness_data['restingHeartRate']=str(random.randint(60,100))
+    fitness_data['ownedBy']={'_id':'5dab481f2d2c6867c9b4221d'}
+    json_fitness=json.dumps(fitness_data)
+
+    print(json_fitness)
+    url='http://localhost:4200/api/fitness'
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    r = requests.post(url, json_fitness, headers=headers)
