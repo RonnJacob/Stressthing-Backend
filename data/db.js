0237f065624c1496e5f8bea232902db5212c6497
Ronn <jacob.r@husky.neu.edu>
2019-10-24 00:32:39 -0400

    Changed Mongo connection string

--- data/db.js
+++ data/db.js
@@ -1,9 +1,9 @@
 module.exports = function(){
     const mongoose = require('mongoose');
     const databaseName = 'hackumass';
-    var connectionString = 'mongodb://127.0.0.1/';
-         connectionString += databaseName;
-    // var connectionString =
-        // 'mongodb://heroku_ws4h5k2l:5g3ik06q8aujjo1d4fuq15grfl@ds253871.mlab.com:53871/heroku_ws4h5k2l';
-    mongoose.connect(connectionString, { useNewUrlParser: true});
+    // var connectionString = 'mongodb://127.0.0.1/';
+        //  connectionString += databaseName;
+    var connectionString =
+        'mongodb://heroku_qhv01j65:3o893igrn8omrp8l2lfgsq1ik0@ds339348.mlab.com:39348/heroku_qhv01j65';
+    mongoose.connect(connectionString, { useNewUrlParser: true, useMongoClient: true});
 };
